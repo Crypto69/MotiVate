71b3decb91f3a0141c937d7f4f2e749e766f4981
Crypto69 <crypto69au@gmail.com>
2025-05-20 17:47:31 +1000

    Initial Commit

---
+++ MotiVate/MotiVateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotiVateApp.swift
+//  MotiVate
+//
+//  Created by Chris Venter on 20/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct MotiVateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
